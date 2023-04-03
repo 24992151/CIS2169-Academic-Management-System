842863f8093c1f9f08c3fd1ff36ac3f418327604
24992151 <24992151@edgehill.ac.uk>
2023-04-03 11:50:07 +0100

    03/04/2023 Commit

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 var pageCounter = 1;
 var moduleContainer = document.getElementById('module-info');
 var btn = document.getElementById("btn");
+var createModuleForm = document.getElementById("create-module-form");
+var moduleDisplay = document.getElementById("module-display");
+var submitModuleBtn = document.getElementById("submit-module-btn");
 
 btn.addEventListener("click", function(){
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://raw.githubusercontent.com/Edge-Hill-Univeristy-Web/CIS2169-Academic-Management-System/main/module-'+ pageCounter +'.json');
   ourRequest.onload = function(){
-    //console.log(ourRequest.responseText);
     var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
     renderHTML(ourData);
   };
   ourRequest.send();
-pageCounter++;
-if (pageCounter > 3){
-//btn.classList.add("hide-me");
-  btn.disabled = true;
-}
+  pageCounter++;
+  if (pageCounter > 3){
+    btn.disabled = true;
+  }
 });
 
 function renderHTML(data){
   var htmlString = "";
 
   for(i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
+    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assessments ";
     for(ii = 0; ii < data[i].Module.Assignment.length; ii++){
       if (ii == 0){
         htmlString += data[i].Module.Assignment[ii];
@@ -60,5 +60,38 @@ function renderHTML(data){
     htmlString += '.</p>';
   }
   moduleContainer.insertAdjacentHTML('beforeend', htmlString);
+}
 
+// displays the new module information
+function displayModuleInfo(id, name, hours, learningOutcomes, credits) {
+  document.getElementById("module-display-id").textContent = id;
+  document.getElementById("module-display-name").textContent = name;
+  document.getElementById("module-display-hours").textContent = hours;
+  document.getElementById("module-display-learning-outcomes").textContent = learningOutcomes;
+  document.getElementById("module-display-credits").textContent = credits;
+}
+
+var submitModuleBtn = document.getElementById("submit-module-btn");
+submitModuleBtn.addEventListener("click", function(event) {
+  event.preventDefault(); //Had to add this as it kept resetting the form almost immediately
+
+  var moduleId = document.getElementById("module-id").value;
+  var moduleName = document.getElementById("module-name").value;
+  var moduleHours = document.getElementById("module-hours").value;
+  var learningOutcomes = document.getElementById("module-learning-outcomes").value;
+  var credits = document.getElementById("module-credits").value;
+  displayModuleInfo(moduleId, moduleName, moduleHours, learningOutcomes, credits);
+
+  var moduleDisplaySection = document.getElementById("module-display");
+  moduleDisplaySection.classList.remove("hidden");
+});
+
+function createModule(id, name, hours, learningOutcomes, credits) {
+  var newModule = {
+    "ID": id,
+    "Name": name,
+    "Hours": hours,
+    "Learning Outcomes": learningOutcomes,
+    "Credits": credits
+  };
 }
